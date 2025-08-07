c5ca61520813d2c8c9a6a7d9ed84d9081b634c25
rociolobruno17 <rociolobruno1@gmail.com>
2025-08-07 19:34:22 -0300

    Agregué mis rede al footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
-// src/components/Footer.jsx
-
 import { Box, Typography, Link } from "@mui/material";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 export default function Footer() {
   return (
@@ -18,12 +19,49 @@ export default function Footer() {
       <Typography variant="body2">
         © {new Date().getFullYear()} Fílmico — Todos los derechos reservados.
       </Typography>
+
       <Typography variant="body2" mt={1}>
-        Hecho con ❤️ por{" "}
-        <Link href="https://tulink.dev" target="_blank" rel="noopener" color="#02FFA1" underline="hover">
-          Rocio Lobruno | Diseñadora UX/UI & Frontend Developer
+        Hecho por{" "}
+        <Link
+          href="https://www.behance.net/lobruno"
+          target="_blank"
+          rel="noopener"
+          color="#02FFA1"
+          underline="hover"
+        >
+          Rocio Lobruno | UX/UI Designer & Frontend Developer
         </Link>
       </Typography>
+
+      <Box
+        mt={2}
+        sx={{ display: "flex", justifyContent: "center", gap: 2 }}
+      >
+        <Link
+          href="https://www.linkedin.com/in/rocio-lobruno/"
+          target="_blank"
+          rel="noopener"
+          color="inherit"
+        >
+          <LinkedInIcon fontSize="medium" />
+        </Link>
+        <Link
+          href="https://www.instagram.com/ui.rociolobruno/"
+          target="_blank"
+          rel="noopener"
+          color="inherit"
+        >
+          <InstagramIcon fontSize="medium" />
+        </Link>
+        <Link
+          href="https://github.com/rociolobruno17"
+          target="_blank"
+          rel="noopener"
+          color="inherit"
+        >
+          <GitHubIcon fontSize="medium" />
+        </Link>
+      </Box>
     </Box>
   );
 }
